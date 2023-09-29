bdf8fd75d44c394cb2fb86f4bcf8525ef461591d
Hnichm <125336099+Hnichm@users.noreply.github.com>
2023-09-29 13:15:54 -0500

    Prettied up

--- Digital Clock/app.js
+++ Digital Clock/app.js
@@ -1,3 +1,5 @@
+// Clock
+
 let hours = document.querySelector(".hours");
 let minutes = document.querySelector(".minutes");
 let seconds = document.querySelector(".seconds");
@@ -13,4 +15,29 @@ setInterval(() => {
     (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 }, 1_000);
 
-let currentTime = new Date();
+// Decoration
+let leftDecoration = document.querySelector(".decoration-left");
+let rootFontSize = parseFloat(
+  getComputedStyle(document.documentElement).getPropertyValue("font-size")
+);
+let topPositionPx = parseFloat(
+  getComputedStyle(leftDecoration).getPropertyValue("top")
+);
+let topPosition = topPositionPx / rootFontSize;
+let goingUp = true;
+
+setInterval(() => {
+  if (goingUp) {
+    topPosition -= 0.05;
+  } else {
+    topPosition += 0.05;
+  }
+
+  if (topPosition <= 10 || topPosition >= 18) {
+    goingUp = !goingUp;
+  }
+
+  let newTopPositionPx = topPosition * rootFontSize;
+
+  leftDecoration.style.top = `${newTopPositionPx}px`;
+}, 5);
