e6fa395f895ac054d2176a0be3089e362c8789bb
hnichm <nmhcoding@gmail.com>
2024-04-18 20:26:29 -0400

    Added touch event listeners in script.js Changed title in index.html

--- Canvas/Particles - Constellations/script.js
+++ Canvas/Particles - Constellations/script.js
@@ -5,29 +5,35 @@ canvas.height = window.innerHeight;
 const particlesArray = [];
 let hue = 0;
 
-window.addEventListener("resize", function () {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-});
-
 const mouse = {
   x: undefined,
   y: undefined,
 };
+
+let particlesToCreate = 0;
+
 canvas.addEventListener("click", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
-  for (let i = 0; i < 100; i++) {
-    particlesArray.push(new Particle());
-  }
+  particlesToCreate = 100;
 });
 
 canvas.addEventListener("mousemove", function (event) {
   mouse.x = event.x;
   mouse.y = event.y;
-  for (let i = 0; i < 5; i++) {
-    particlesArray.push(new Particle());
-  }
+  particlesToCreate = 5;
+});
+
+canvas.addEventListener("touchstart", function (event) {
+  mouse.x = event.touches[0].clientX;
+  mouse.y = event.touches[0].clientY;
+  particlesToCreate = 100;
+});
+
+canvas.addEventListener("touchmove", function (event) {
+  mouse.x = event.touches[0].clientX;
+  mouse.y = event.touches[0].clientY;
+  particlesToCreate = 5;
 });
 
 class Particle {
@@ -39,11 +45,13 @@ class Particle {
     this.speedY = Math.random() * 3 - 1.5;
     this.color = `hsl(${hue}, 100%, 50%)`;
   }
+
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
     if (this.size > 0.2) this.size -= 0.1;
   }
+
   draw() {
     ctx.fillStyle = this.color;
     ctx.beginPath();
@@ -53,17 +61,23 @@ class Particle {
 }
 
 function handleParticles() {
+  if (particlesToCreate > 0) {
+    for (let i = 0; i < particlesToCreate; i++) {
+      particlesArray.push(new Particle());
+    }
+    particlesToCreate = 0;
+  }
+
   for (let i = 0; i < particlesArray.length; i++) {
     particlesArray[i].update();
     particlesArray[i].draw();
-    // For every particle in the array, cycle through remaining particles in the same array, compare distances
+
     for (let j = i; j < particlesArray.length; j++) {
-      // pythagorean theorem to calculate distance between particles
-      const distanceX = particlesArray[i].x - particlesArray[j].x;
-      const distanceY = particlesArray[i].y - particlesArray[j].y;
-      const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-      if (distance < 100 && distance > 20) {
-        // drawing line
+      const dx = particlesArray[i].x - particlesArray[j].x;
+      const dy = particlesArray[i].y - particlesArray[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < 100) {
         ctx.beginPath();
         ctx.strokeStyle = particlesArray[i].color;
         ctx.lineWidth = 0.2;
@@ -73,6 +87,7 @@ function handleParticles() {
         ctx.closePath();
       }
     }
+
     if (particlesArray[i].size <= 0.3) {
       particlesArray.splice(i, 1);
       i--;
@@ -82,10 +97,9 @@ function handleParticles() {
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // ctx.fillStyle = "rgba(0, 0, 0, 0.02)";
-  // ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
   hue += 2;
   requestAnimationFrame(animate);
 }
+
 animate();
